7cf2794a52bb0c86808d99d5f5f6b6360ab67921
yixuan112-coder <yixuan071112@outlook.com>
2026-07-01 21:09:20 +0800

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Yixuan's website";
+export const SITE_TITLE = "yixuan's blog";
 export const SITE_DESCRIPTION =
 	'Personal website and blog of Yixuan Xie (谢奕轩) — NTU CS student, projects, blog, and contact.';
 
